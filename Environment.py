701af7350068e2323f0b3c09ce17930a012f880c
ilektram <elektrapapazoglou@gmail.com>
2017-02-09 21:39:25 +0000

    commit

--- Environment.py
+++ Environment.py
@@ -1,40 +1,88 @@
-import gym
 import numpy as np
+import gym
+import tempfile
 
-env = gym.make('FrozenLake-v0')
-# Initialize table with all zeros
-Q = np.zeros([env.observation_space.n, env.action_space.n])
-# Set learning parameters
-lr = .85
-y = .99
-num_episodes = 2000
-# Create lists to contain total rewards and steps per episode
-# jList = []
-rList = []
-for i in range(num_episodes):
-    # Reset environment and get first new observation
-    s = env.reset()
-    rAll = 0
-    d = False
-    j = 0
-    # The Q-Table learning algorithm
-    while j < 99:
-        env.render()
-        print(s)
-        j+=1
-        # Choose an action by greedily (with noise) picking from Q table
-        a = np.argmax(Q[s,:] + np.random.randn(1,env.action_space.n)*(1./(i+1)))
-        # Get new state and reward from environment
-        s1,r,d,_ = env.step(a)
-        # Update Q-Table with new knowledge
-        Q[s,a] = Q[s,a] + lr*(r + y*np.max(Q[s1,:]) - Q[s,a])
-        rAll += r
-        s = s1
-        if d == True:
-            break
-    # jList.append(j)
-    rList.append(rAll)
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten
+from keras.layers.core import Reshape
+from keras.layers.embeddings import Embedding
+from keras.optimizers import SGD
 
-print("Score over time: " +  str(sum(rList)/num_episodes))
-print("Final Q-Table Values")
-print(Q)
+from rl.agents import DQNAgent
+from rl.memory import SequentialMemory
+from rl.policy import BoltzmannQPolicy
+
+
+
+
+ENV_NAME = 'FrozenLake8x8-v0'
+gym.undo_logger_setup()
+
+
+# Get the environment and extract the number of actions.
+env = gym.make(ENV_NAME)
+np.random.seed(123)
+env.seed(123)
+
+if "n" in dir(env.action_space):
+    nb_actions = env.action_space.n
+elif "shape" in dir(env.action_space):
+    nb_actions = env.action_space.shape[0]
+else:
+    raise TypeError("No n or shape in action_space :-O")
+
+
+if "n" in dir(env.observation_space):
+    in_shape = env.observation_space.n
+elif "shape" in dir(env.observation_space):
+    in_shape = (1,) + env.observation_space.shape
+else:
+    raise TypeError("No n or shape in observation space :-O")
+
+# Build all necessary models: V, mu, and L networks.
+model = Sequential()
+model.add(Embedding(in_shape, nb_actions, input_length=1))
+model.add(Activation('relu'))
+model.add(Dense(nb_actions))
+model.add(Activation('linear'))
+model.add(Reshape((nb_actions,)))
+print("Compiling the following model:")
+print(model.summary(), "\n")
+
+# Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
+# even the metrics!
+memory = SequentialMemory(limit=50000, window_length=1)
+policy = BoltzmannQPolicy()
+dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=50,
+               target_model_update=1e-2, policy=policy)
+dqn.compile(SGD(lr=.1))
+
+try:
+    dqn.load_weights('dqn_{}_weights.h5f'.format(ENV_NAME))
+    print("\n Loaded existing weights.\n")
+except Exception as e:
+    print(e)
+    pass
+
+temp_folder = tempfile.mkdtemp()
+# env.monitor.start(temp_folder)
+# env = gym.wrappers.Monitor(env, temp_folder)
+
+# Okay, now it's time to learn something! We visualize the training here for show, but this
+# slows down training quite a lot. You can always safely abort the training prematurely using
+# Ctrl + C.
+dqn.fit(env, nb_steps=1e5, visualize=True, verbose=2, log_interval=10000)
+
+# After training is done, we save the final weights.
+dqn.save_weights('dqn_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
+
+# Finally, evaluate our algorithm for 5 episodes.
+print("Testing for 5 episodes:")
+dqn.test(env, nb_episodes=5, visualize=True)
+
+# env.monitor.close()
+
+upload = input("Upload? (y/n)")
+if upload == "y":
+    gym.upload(temp_folder, api_key='YOUR_OWN_KEY')
+print("DONE")
---
+++ SimpleAgent.py
@@ -0,0 +1,64 @@
+
+import os
+
+import gym
+import numpy as np
+
+
+
+ENV_NAME = 'FrozenLake8x8-v0'
+gym.undo_logger_setup()
+
+# Get the environment and extract the number of actions.
+env = gym.make(ENV_NAME)
+np.random.seed(123)
+env.seed(123)
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+# Initialize table with all zeros
+Q = np.ones([env.observation_space.n, env.action_space.n]) * 0
+
+# Set learning parameters
+lr = .95
+gamma = .85
+num_episodes = 10000
+
+# Create lists to contain total rewards and steps per episode
+tList = []
+rList = []
+
+for i in range(num_episodes):
+
+    # Reset environment and get first new observation
+    state = env.reset()
+    env.render()
+    rAll = 0
+    t = 0
+
+    # The Q-learning algorithm
+    while t < 99:
+        t += 1
+        print("State:", state)
+
+        # Choose an action by greedily (with noise) picking from Q table
+        action = np.argmax(Q[state, :] + np.random.randn(1, env.action_space.n) * (1. / (i + 1)))
+
+        # Get new state and reward from environment
+        state1, reward, done, info = env.step(action)
+
+        # Update Q-Table with new knowledge
+        Q[state, action] = Q[state, action] + lr * (reward + gamma * np.max(Q[state1, :]) - Q[state, action])
+        rAll += reward
+        state = state1
+
+        if done:
+            break
+
+    tList.append(t)
+    rList.append(rAll)
+    print("End of Episode", i, "\n")
+
+print("Final Q table:")
+print(Q)
+print("Score over time: " + str(sum(rList)/num_episodes))
